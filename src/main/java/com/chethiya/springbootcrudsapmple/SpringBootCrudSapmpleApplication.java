552a93bc91a6d9bca2558fa20cf3bcfa4d1be945
lahiruchethiya <lahiruchethiya.jayasinghe@gmail.com>
2022-06-05 13:09:17 +0530

    Initial Commit

---
+++ src/main/java/com/chethiya/springbootcrudsapmple/SpringBootCrudSapmpleApplication.java
@@ -0,0 +1,13 @@
+package com.chethiya.springbootcrudsapmple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudSapmpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudSapmpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chethiya/springbootcrudsapmple/SpringBootCrudSapmpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chethiya.springbootcrudsapmple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudSapmpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
